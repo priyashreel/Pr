fa0a5f7f17d47ad9a0718e1dbf2ed934b1619e29
priyashreel <priyashreel.22is@saividya.ac.in>
2025-10-17 09:22:05 +0530

    Introduction

---
+++ python_practice.py
@@ -0,0 +1,2 @@
+print("welcome to Python practice session")
+print("hello! this is priyashree , am here to showcase myself about my practice sessions in python , am very excited to share with you all...")
